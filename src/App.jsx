730a1599d727f6acc1583b1b4c1e0191c755b02e
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2024-09-21 12:03:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import explorer from './Data/explorer'
 import Folder from './Folder/Folder';
+import useTraverse from './hooks/useTraverse';
 function App() {
   const [explorerData, setExplorerData]=useState(explorer);
+  const { insertNode } = useTraverse();
   console.log(explorerData);
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item ,isFolder);
+    setExplorerData(finalTree)
+  }
   return (
     <>
-      <Folder explorer={explorerData}/>
+      <Folder explorer={explorerData} handleInsertNode={handleInsertNode}/>
       <span>Folder</span>
     </>
   )
