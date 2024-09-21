60a58f10a82b13debb12ff14c7969cfbfff05aa2
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2024-09-21 12:03:08 +0530

    Create useTraverse.js

---
+++ src/hooks/useTraverse.js
@@ -0,0 +1,21 @@
+const useTraverse = () => {
+  function insertNode(tree, folderId, item, isFolder) {
+    if (tree.id === folderId && tree.isFolder) {
+      tree.items.unshift({
+        id: new Date().getTime(),
+        name: item,
+        isFolder,
+        items: [],
+      });
+      return tree;
+    }
+    let latestNode = [];
+    latestNode = tree.items.map((x) => {
+      return insertNode(x, folderId, item, isFolder);
+    });
+    return { ...tree, items: latestNode };
+  }
+  return { insertNode };
+};
+
+export default useTraverse;
